55f327ae562af9dc47499566dcb0f38b56b9821d
Anderson <anderson.assis@outlook.com>
2022-09-07 18:19:56 -0300

    Adicionado os métodos para criar, excluir, deletar

--- Program.cs
+++ Program.cs
@@ -8,6 +8,51 @@ app.MapGet("/", () => "Api de contatos!");
 //app.MapGet("/", () => new Contato());
 
 // rota para todos os contatos
-app.MapGet("/todoscontatos", () => DadosDeContatos.listaDeContatos);
+app.MapGet("/contato", () => DadosDeContatos.listaDeContatos);
+
+// rota para trazer um contato
+app.MapGet("/contato/{id}", (Guid id) =>
+    DadosDeContatos.listaDeContatos.Find(contato => contato.Id == id));
+
+// rota para criar um contato
+app.MapPost("/contato", (Contato contato) =>
+{
+    DadosDeContatos.listaDeContatos.Add(contato);
+    Results.Ok("Contato criado");
+
+});
+
+// rota para deletar um contato
+app.MapDelete("/contato/{id}", (Guid id) =>
+{
+
+    var contatoParaDeletar = DadosDeContatos.listaDeContatos.FirstOrDefault(contato => contato.Id == id);
+    if (contatoParaDeletar != null)
+    {
+        DadosDeContatos.listaDeContatos.Remove(contatoParaDeletar);
+        Results.Ok();
+    }
+    else
+    {
+        Results.NotFound("Nao foi encontrado um contato");
+    }
+
+});
+
+// rota para atualizar um contato
+app.MapPut("/contato/atualizar/{id}", (Guid id, Contato contatoRecebidoParaAtualizar) =>
+{
+
+    var contatoParaAtualizar = DadosDeContatos.listaDeContatos.Find(contato => contato.Id == id);
+    var indiceDoContatoParaAtualizar=DadosDeContatos.listaDeContatos.FindIndex(contato =>contato.Id==id);
+
+    if (contatoParaAtualizar != null)
+    {
+        contatoParaAtualizar.Nome = contatoRecebidoParaAtualizar.Nome;
+        contatoParaAtualizar.Telefone = contatoRecebidoParaAtualizar.Telefone;
+        DadosDeContatos.listaDeContatos.Insert(indiceDoContatoParaAtualizar,contatoParaAtualizar);
+    }
+
+});
 
 app.Run();
